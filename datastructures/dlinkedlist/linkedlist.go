a8383105b8d55eae4301223124e0102404513f32
Per <per@dubgeiser.net>
2023-09-28 08:23:45 +0200

    Feeble attempt at structuring doubly linked list

---
+++ datastructures/dlinkedlist/linkedlist.go
@@ -0,0 +1,10 @@
+package dlinkedlist
+
+import (
+	"kata/datastructures/node"
+)
+
+type DLinkedList struct {
+	head *node.DNode
+	tail *node.DNode
+}
--- datastructures/node/node.go
+++ datastructures/node/node.go
@@ -9,3 +9,15 @@ func New(v any) *Node {
 	n := Node{v, nil}
 	return &n
 }
+
+// I want to call this 'Node' too :-(
+type DNode struct {
+	Value    any
+	Next     *Node
+	Previous *Node
+}
+
+func DNew(v any) *DNode {
+	n := DNode{v, nil, nil}
+	return &n
+}
